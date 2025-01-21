8e014c6557f4568f92442f604a8b663857bc0b36
thoaidc <dcthoai1023@gmail.com>
2025-01-21 16:53:52 +0700

    add comment for properties config class

--- src/main/java/com/dct/base/config/properties/Datasource.java
+++ src/main/java/com/dct/base/config/properties/Datasource.java
@@ -1,9 +1,24 @@
 package com.dct.base.config.properties;
 
 import com.dct.base.constants.PropertiesConstants;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * Contains the configuration for the datasource, such as the database connection url, username, password, etc<p>
+ * When the application starts, Spring will automatically create an instance of this class
+ * and load the values from configuration files like application.properties or application.yml <p>
+ *
+ * {@link ConfigurationProperties} helps Spring map config properties to fields,
+ * instead of using @{@link Value} for each property individually <p>
+ *
+ * {@link PropertiesConstants#DATASOURCE} decides the prefix for the configurations that will be mapped <p>
+ *
+ * See <a href="">application-test.yml</a> for detail
+ *
+ * @author thoaidc
+ */
 @Configuration
 @ConfigurationProperties(prefix = PropertiesConstants.DATASOURCE)
 public class Datasource {
